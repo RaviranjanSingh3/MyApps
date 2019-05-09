74e3e7e5c8e4e1f8e43ce23b2528d1abc5f0bab5
Ravi <Ravi@192.168.0.13>
2019-05-09 22:32:40 +0530

    This is  commoit

---
+++ src/com/app/Admin.java
@@ -0,0 +1,15 @@
+package com.app;
+
+public class Admin {
+private int adminId;
+
+
+public int getAdminId() {
+	return adminId;
+}
+
+public void setAdminId(int adminId) {
+	this.adminId = adminId;
+}
+
+}
